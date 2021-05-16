5887eb7ee6da32cb36a0963c07c6e131533c237b
Yobin00 <yobinpun.kumar@infodevelopers.com.np>
2021-05-16 10:36:00 +0545

    Buf fix on props history and api key

--- src/apikeys.js
+++ src/apikeys.js
@@ -1,5 +1,7 @@
-export default {
+const apikeys = {
   USER_ID: process.env.REACT_APP_USER_ID,
   TEMPLATE_ID: process.env.REACT_APP_TEMPLATE_ID,
   SERVICE_ID: process.env.REACT_APP_SERVICE_ID,
 };
+
+export default apikeys;
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -55,7 +55,7 @@ const ContactForm = (props:any) => {
                 }
                , 3000);
             }
-        }, [success])
+        }, [success,props])
 
     return (
         <div>
